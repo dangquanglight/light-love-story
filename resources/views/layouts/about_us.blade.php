32b6b54aa157d650cd39eb92c138db3c5067a8fe
Quang Le Dang <ldangquang@tma.com.vn>
2015-12-16 11:49:07 +0700

    divide main layout into many parts

---
+++ resources/views/layouts/about_us.blade.php
@@ -0,0 +1,179 @@
+<section id="aboutthem" class="white">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="heading">
+                    <h2 class="wow fadeInUp underline padtop50" data-wow-delay="0.5s">Welcome Everyone</h2>
+
+                    <p class="wow fadeInUp" data-wow-delay="0.7s">
+                        Welcome to adams' & Eves' website where we proudly tell and recored our love story with all its
+                        ups and downs with all great moments and happy events.
+                    </p>
+                </div>
+            </div>
+            <div class="col-md-12">
+                <div class="col-md-6">
+                    <ul class="ch-grid man wow flipInX" data-wow-delay="0.5s">
+                        <li>
+                            <div class="ch-item ch-img-1">
+                                <div class="ch-info">
+                                    <h3>I'm Adam</h3>
+
+                                    <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots
+                                        in a piece of classical Latin literature from 45 BC, making it over 2000 years
+                                        old.</p>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <div class="and">&amp;</div>
+                <div class="col-md-6">
+                    <ul class="ch-grid woman wow flipInX" data-wow-delay="0.5s">
+                        <li>
+                            <div class="ch-item ch-img-2">
+                                <div class="ch-info">
+                                    <h3>I'm Eve</h3>
+
+                                    <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots
+                                        in a piece of classical Latin literature from 45 BC, making it over 2000 years
+                                        old.</p>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--Loveline-->
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="heading">
+                    <h2 class="wow fadeInUp underline padtop50" data-wow-delay="0.5s">Our Loveline</h2>
+
+                    <p class="wow fadeInUp" data-wow-delay="0.7s">
+                        <strong>Love Moments</strong> are special and it deserve to be recorded and to be always
+                        remembered! our loveline is just summerize some of our important and happy moments.
+                    </p>
+                </div>
+                <ul class="timeline">
+                    <li>
+                        <div class="timeline-badge wow fadeInUp" data-wow-delay="0.5s"><i class="icon icolove-lips"></i>
+                        </div>
+                        <div class="timeline-panel wow fadeInLeft" data-wow-delay="0.7s">
+                            <div class="timeline-heading">
+                                <h4 class="timeline-title">Our First Kiss</h4>
+
+                                <p>
+                                    <small class="text-muted"><i class="glyphicon glyphicon-time"></i> 24 May 2010 - By
+                                        Adam
+                                    </small>
+                                </p>
+                            </div>
+                            <div class="timeline-body">
+                                <p>
+                                    It wasn't that long, and it certainly wasn't the kind of kiss you see in movies
+                                    these days, but it was wonderful in its own way, and all I can remember about the
+                                    moment is that when our lips touched, I knew the memory would last forever.
+                                </p>
+                            </div>
+                        </div>
+                    </li>
+
+                    <li class="timeline-inverted negative100">
+                        <div class="timeline-badge time wow fadeInUp" data-wow-delay="0.5s"><i
+                                    class="icon icolove-ring"></i></div>
+                        <div class="timeline-panel wow fadeInRight" data-wow-delay="0.7s">
+                            <div class="timeline-heading">
+                                <h4 class="timeline-title">Got Engaged</h4>
+
+                                <p>
+                                    <small class="text-muted"><i class="glyphicon glyphicon-time"></i> 20 Dec 2010 - By
+                                        Eve
+                                    </small>
+                                </p>
+                            </div>
+                            <div class="timeline-body">
+                                <p>
+                                    Her boyfriend gives her a Mercedes, [her friends] say, 'Oh, that's nice.' But her
+                                    boyfriend gives her a diamond, they say, 'Oh, he's serious.' It's not just the gift
+                                    of love-it's the gift of commitment. She's not jumping up and down because she got a
+                                    diamond ring but because she got a guy!
+                                </p>
+                            </div>
+                        </div>
+                    </li>
+
+                    <li class="negative75">
+                        <div class="timeline-badge wow fadeInUp" data-wow-delay="0.5s"><i
+                                    class="icon icolove-married"></i></div>
+                        <div class="timeline-panel wow fadeInLeft" data-wow-delay="0.7s">
+                            <div class="timeline-heading">
+                                <h4 class="timeline-title">Got Married</h4>
+
+                                <p>
+                                    <small class="text-muted"><i class="glyphicon glyphicon-time"></i> 24 May 2011 - By
+                                        Adam
+                                    </small>
+                                </p>
+                            </div>
+                            <div class="timeline-body">
+                                <p>
+                                    Marriage has made me a lot happier and I'm deeply in love with my wife, and I thank
+                                    God for her every day.
+                                </p>
+                            </div>
+                        </div>
+                    </li>
+
+                    <li class="timeline-inverted negative50">
+                        <div class="timeline-badge time wow fadeInUp" data-wow-delay="0.5s"><i
+                                    class="icon icolove-pregnancy"></i></div>
+                        <div class="timeline-panel wow fadeInRight" data-wow-delay="0.7s">
+                            <div class="timeline-heading">
+                                <h4 class="timeline-title">I'm Pregnant</h4>
+
+                                <p>
+                                    <small class="text-muted"><i class="glyphicon glyphicon-time"></i> 12 Jan 2012 - By
+                                        Eve
+                                    </small>
+                                </p>
+                            </div>
+                            <div class="timeline-body">
+                                <p>
+                                    Of course I can do this. I'm pregnant, not brain-damaged. My condition doesn't
+                                    change my personality.
+                                </p>
+                            </div>
+                        </div>
+                    </li>
+
+                    <li class="negative75">
+                        <div class="timeline-badge wow fadeInUp" data-wow-delay="0.5s"><i
+                                    class="icon icolove-babyboy"></i></div>
+                        <div class="timeline-panel wow fadeInLeft" data-wow-delay="0.7s">
+                            <div class="timeline-heading">
+                                <h4 class="timeline-title">Welcome Michael!</h4>
+
+                                <p>
+                                    <small class="text-muted"><i class="glyphicon glyphicon-time"></i> 1 Oct 2012 - By
+                                        Adam
+                                    </small>
+                                </p>
+                            </div>
+                            <div class="timeline-body">
+                                <p>
+                                    Welcome Our new little family member, he is so cute and we still afraid to hurt him
+                                    as we really don't have experience with newborn, excuse us Michael!
+                                </p>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="separator"></div>
+</section>
---
+++ resources/views/layouts/family.blade.php
@@ -0,0 +1,136 @@
+<section id="family" class="white">
+    <div class="container">
+        <div class="row">
+            <!--Our Family-->
+            <div class="col-md-12">
+                <div class="heading">
+                    <h2 class="wow fadeInUp underline padtop50" data-wow-delay="0.5s">Family Members</h2>
+
+                    <p class="wow fadeInUp" data-wow-delay="0.7s">Contrary to popular belief, Lorem Ipsum is not simply
+                        random text. It has roots in a piece of classical Latin literature from 45 BC, making it over
+                        2000 years old.</p>
+                </div>
+                <div class="col-md-4 man-family">
+                    <h3>Adams' Family</h3>
+
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam rhoncus, leo at pretium
+                        commodo</p>
+
+                    <div id="owl-man-family" class="owl-carousel wow fadeInUp" data-wow-delay="0.7s">
+                        <div class="owl-item">
+                            <div class="family-member">
+                                <img class="img-responsive" src="images/family-1.jpg" alt="team"/>
+                                <h4>Bill Swan</h4>
+                                <h5>Father</h5>
+                            </div>
+                        </div>
+
+                        <div class="owl-item">
+                            <div class="family-member">
+                                <img class="img-responsive" src="images/family-2.jpg" alt="team"/>
+                                <h4>Christina lewis</h4>
+                                <h5>mother</h5>
+                            </div>
+                        </div>
+
+                        <div class="owl-item">
+                            <div class="family-member">
+                                <img class="img-responsive" src="images/family-5.jpg" alt="team"/>
+                                <h4>Every Swan</h4>
+                                <h5>Brother</h5>
+                            </div>
+                        </div>
+
+                        <div class="owl-item">
+                            <div class="family-member">
+                                <img class="img-responsive" src="images/family-6.jpg" alt="team"/>
+                                <h4>lexy Swan</h4>
+                                <h5>Sister</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <img class="img-responsive" src="images/couple-standing.jpg" alt="couple"/>
+                </div>
+                <div class="col-md-4 woman-family">
+                    <h3>Eves' Family</h3>
+
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam rhoncus, leo at pretium
+                        commodo</p>
+
+                    <div id="owl-woman-family" class="owl-carousel wow fadeInUp" data-wow-delay="0.7s">
+                        <div class="owl-item">
+                            <div class="family-member">
+                                <img class="img-responsive" src="images/family-3.jpg" alt="team"/>
+                                <h4>Martin Nos</h4>
+                                <h5>Father</h5>
+                            </div>
+                        </div>
+
+                        <div class="owl-item">
+                            <div class="family-member">
+                                <img class="img-responsive" src="images/family-4.jpg" alt="team"/>
+                                <h4>Marria Grace</h4>
+                                <h5>Mother</h5>
+                            </div>
+                        </div>
+
+                        <div class="owl-item">
+                            <div class="family-member">
+                                <img class="img-responsive" src="images/family-7.jpg" alt="team"/>
+                                <h4>Owen Martin</h4>
+                                <h5>Brother</h5>
+                            </div>
+                        </div>
+
+                        <div class="owl-item">
+                            <div class="family-member">
+                                <img class="img-responsive" src="images/family-8.jpg" alt="team"/>
+                                <h4>Hanna Martin</h4>
+                                <h5>Sister</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!--Fun Facts-->
+                <div id="startcounting" class="row count">
+                    <div class="col-md-12">
+                        <div class="heading wow fadeInUp" data-wow-delay="0.5s">
+                            <h2 class="wow fadeIn underline padtop50">Fun Facts</h2>
+
+                            <p class="wow fadeInUp" data-wow-delay="0.7s">Contrary to popular belief, Lorem Ipsum is not
+                                simply random text. It has roots in a piece of classical Latin literature from 45 BC,
+                                making it over 2000 years old.</p>
+                        </div>
+                        <div class="counting col-ms-12 col-sm-6 col-md-3 wow flipInY" data-wow-delay="0.7s">
+                            <div class="fun-facts ico"><span class="icon icolove-movie"></span></div>
+                            <span class="counts" data-from="0" data-to="300" data-speed="5000"
+                                  data-refresh-interval="50">0</span>
+                            <h5>Movies Watched</h5>
+                        </div>
+                        <div class="counting col-ms-12 col-sm-6 col-md-3 wow flipInY" data-wow-delay="0.7s">
+                            <div class="fun-facts ico"><span class="icon icolove-coffee"></span></div>
+                            <span class="counts" data-from="0" data-to="550" data-speed="5000"
+                                  data-refresh-interval="50">0</span>
+                            <h5>Cup of Coffee</h5>
+                        </div>
+                        <div class="counting col-ms-12 col-sm-6 col-md-3 wow flipInY" data-wow-delay="0.7s">
+                            <div class="fun-facts ico"><span class="icon icolove-party"></span></div>
+                            <span class="counts" data-from="0" data-to="150" data-speed="5000"
+                                  data-refresh-interval="50">0</span>
+                            <h5>Fun Parties</h5>
+                        </div>
+                        <div class="counting col-ms-12 col-sm-6 col-md-3 wow flipInY" data-wow-delay="0.7s">
+                            <div class="fun-facts ico"><span class="icon icolove-gamepad"></span></div>
+                            <span class="counts" data-from="0" data-to="410" data-speed="5000"
+                                  data-refresh-interval="50">0</span>
+                            <h5>Game Played</h5>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</section>
