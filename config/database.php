4599bb28e583fd7978020ff8a7a38a214e6e88d7
Quang Le Dang <dangquang.light@gmail.com>
2015-12-19 01:10:57 +0700

    Update DB config with env

--- config/database.php
+++ config/database.php
@@ -47,10 +47,10 @@ return [
     'connections' => [
         'mysql' => [
             'driver' => 'mysql',
-            'host' => 'localhost',
-            'database' => 'love_dev',
-            'username' => 'root',
-            'password' => 'root',
+            'host'      => env('DB_HOST', 'localhost'),
+            'database'  => env('DB_DATABASE', 'forge'),
+            'username'  => env('DB_USERNAME', 'forge'),
+            'password'  => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
